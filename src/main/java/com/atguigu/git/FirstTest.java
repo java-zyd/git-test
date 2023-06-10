ae0d80026357c4c390be63a11ef73b74928f7201
zyd <1804842860@qq.com>
2023-06-10 19:30:30 +0800

    master commit2

--- src/main/java/com/atguigu/git/FirstTest.java
+++ src/main/java/com/atguigu/git/FirstTest.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class FirstTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
-        System.out.println("hello,git2");
+        System.out.println("hello,haha");
         System.out.println("hello,git3");
         System.out.println("hello git4");
     }
