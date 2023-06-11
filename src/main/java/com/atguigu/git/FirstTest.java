decd5b578c07502ded853ba043480a544b4762a4
java-zyd <136142199+java-zyd@users.noreply.github.com>
2023-06-11 09:06:35 +0800

    pull commit

--- src/main/java/com/atguigu/git/FirstTest.java
+++ src/main/java/com/atguigu/git/FirstTest.java
@@ -10,5 +10,6 @@ public class FirstTest {
         System.out.println("z y d");
         System.out.println("dsb");
         System.out.println("push test");
+        System.out.println("pull test")
     }
 }
